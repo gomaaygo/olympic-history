582b97461b284616e51e82e5dc4db2692dd7322e
Maiury S. Garcia <maiurygarcia@gmail.com>
2022-05-11 08:46:13 -0300

    Updated admin

--- event/admin.py
+++ event/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
+from .models import Game, City, Sport, Event, AthleteEvent
+
 # Register your models here.
+
+class GameModelAdmin(admin.ModelAdmin):
+    list_display = ['year', 'season']
+
+class CityModelAdmin(admin.ModelAdmin):
+    list_display = ['city']
+
+class SportModelAdmin(admin.ModelAdmin):
+    list_display = ['sport']
+
+class EventModelAdmin(admin.ModelAdmin):
+    list_display = ['event', 'sport', 'city', 'game']
+
+class AthleteEventModelAdmin(admin.ModelAdmin):
+    list_display = ['athlete', 'event', 'weight', 'height', 'age', 'medal']
+
+
+admin.site.register(Game, GameModelAdmin)
+admin.site.register(City, CityModelAdmin)
+admin.site.register(Sport, SportModelAdmin)
+admin.site.register(Event, EventModelAdmin)
+admin.site.register(AthleteEvent, AthleteEventModelAdmin)
