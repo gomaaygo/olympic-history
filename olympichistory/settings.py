1d24b611cecb68ddeef3170f24ad41719f82346d
Maiury S. Garcia <maiurygarcia@gmail.com>
2022-05-11 11:48:27 -0300

    Django filters installed

--- olympichistory/settings.py
+++ olympichistory/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'athlete',
     'event',
 ]
@@ -107,6 +108,11 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}   
 
 
 # Internationalization
