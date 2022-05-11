419a2c81f65779b91a86636bd4332b6cfbca2980
Maiury S. Garcia <maiurygarcia@gmail.com>
2022-05-11 08:31:12 -0300

    Added game model

--- event/models.py
+++ event/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
+
+class Game(models.Model):
+    SEASON_CHOICES = (
+        ("Summer", "Summer"),
+        ("Winter", "Winter"),
+    )
+    year = models.IntegerField(verbose_name="Year", validators=[MinValueValidator(1896)], null=False, blank=False)
+    season = models.CharField(verbose_name="Season", max_length=6, choices=SEASON_CHOICES, null=False, blank=False)
+
+    class Meta:
+        verbose_name = "Game"
+        verbose_name_plural = "Games"
+
+    def __str__(self):
+        return str(self.year) + str(self.season)
