13905019d9dceac0f2efade90125fd18aff28625
Maiury S. Garcia <maiurygarcia@gmail.com>
2022-05-13 22:49:17 -0300

    Added pagination

--- olympichistory/settings.py
+++ olympichistory/settings.py
@@ -113,9 +113,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
 }   
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
